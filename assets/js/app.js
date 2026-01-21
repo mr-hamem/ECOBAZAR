69a89404e529d6775fe3fa5d36508f76b596ffe1
MR HAMEM <hamemapple2018@gmail.com>
2026-01-21 16:47:07 +0600

    added the slick the slide without responsiveness

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+    fade: true,
+    dotsClass: 'my-dots',
+  });
+});
