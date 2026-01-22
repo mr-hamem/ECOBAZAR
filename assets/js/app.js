f20080a29352d9612962ce475f79a0207a8cbf9d
MR HAMEM <hamemapple2018@gmail.com>
2026-01-23 01:16:48 +0600

    made the banner slider and also made it responsive 0 to 475px

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
-  $('.your-class').slick({
+  $('.banner_slider').slick({
+    slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
-    dots: true,
+    autoplaySpeed: 10000000,
     arrows: false,
     fade: true,
-    dotsClass: 'my-dots',
+    cssEase: 'linear',
+    dotsClass: 'banner_sliderDots',
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      },
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+      }
+    }
+  ]
   });
 });
